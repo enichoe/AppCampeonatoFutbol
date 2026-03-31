904365b01da4002d3941709d6d1b453d792a36bc
Ernesto Nicho <enichoe@gmail.com>
2026-03-31 18:31:50 -0500

    first commit

--- src/modules/Dashboard.js
+++ src/modules/Dashboard.js
@@ -17,7 +17,7 @@ export const renderDashboard = (container, view, params) => {
             </svg>
           </div>
           <div>
-            <span class="text-xl font-black text-white tracking-tighter italic block">SportSaaS</span>
+            <span class="text-xl font-black text-white tracking-tighter italic block">Fútbol App</span>
             <span class="text-[9px] font-bold text-slate-500 uppercase tracking-widest -mt-1 block">Admin Console</span>
           </div>
           <button id="closeSidebar" class="md:hidden ml-auto p-2 text-slate-500">
@@ -28,7 +28,7 @@ export const renderDashboard = (container, view, params) => {
         <nav class="flex-grow px-6 mt-4 space-y-2 no-scrollbar overflow-y-auto">
           <button id="navDashboard" class="sidebar-item ${view === 'dashboard' ? 'active' : ''}">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
-            <span>Mis Torneos</span>
+            <span>Mis Campeonatos</span>
           </button>
           
           <button id="navTournaments" class="sidebar-item ${view === 'torneos' || view === 'detalle_torneo' ? 'active' : ''}">
